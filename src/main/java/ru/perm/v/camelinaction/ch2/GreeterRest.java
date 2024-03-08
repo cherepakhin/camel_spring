6ca65326f6ea104b509df0f273b07308d50e19de
Черепахин Василий <vasi.che@gmail.com>
2024-03-08 19:20:56 +0500

    inject bean.xml to spring context

--- src/main/java/ru/perm/v/camelinaction/ch2/GreeterRest.java
+++ src/main/java/ru/perm/v/camelinaction/ch2/GreeterRest.java
@@ -1,6 +1,7 @@
 package ru.perm.v.camelinaction.ch2;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/greet")
 public class GreeterRest {
+    @Autowired
+    GreeterService selectedGreeterService; //TODO: inject
 
-    GreeterService greeterService; //TODO: inject
+//    public GreeterRest(GreeterService selectedGreeterService) {
+//        this.selectedGreeterService = selectedGreeterService;
+//    }
+
 
     @GetMapping("/echo/{message}")
     public String echo(@PathVariable String message) {
@@ -19,6 +25,6 @@ public class GreeterRest {
 
     @GetMapping("/hello")
     public String getHello() {
-        return greeterService.getHello();
+        return selectedGreeterService.getHello();
     }
 }
---
+++ src/main/java/ru/perm/v/camelinaction/ch2/XmlConfiguration.java
@@ -0,0 +1,9 @@
+package ru.perm.v.camelinaction.ch2;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource({"classpath*:beans.xml"})
+public class XmlConfiguration {
+}
